2c7090e1972e9bfc15e70688908f59fe53847e5b
Muhammad Iqbal <qebamagi@gmail.com>
2021-06-01 15:58:26 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# This script intended to automate install NodeExporter
+
+## How to use?
+Run the install.sh as root user and you good to go!. 
+
+## NodeExporter Details?.
+- Port used is: 9100
+- Current version: v1.1.2
+
+## How it works?
+- This script will conver the NodeExporter to script and run basic testing. 
---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+# This script intended to be use to install NodeExporter for prometheus
+#
+# Wrote by qeba-
+#
+# First script is written on 1/6/2021
+# Usage: bash install.sh
+
+echo "----------------------------------------------------------------------------------"
+echo "This script is used to setup NodeExporter Automatically on Linux OS"
+echo "----------------------------------------------------------------------------------"
+read -p "Press Enter key when you ready!/  "
+
+
+checkFolder() {
+if [ -d "./tempInstall" ] 
+    then
+        printf  "\nDirectory Alerady exists, will delete the directory first." 
+        printf  "\nMake sure you backup if there related data in ./tempInstall folder," 
+        printf  "\n"
+        read -p "Press Enter to continue delete or ctrl + c to cancel!.   "
+        rm -rf ./tempInstall
+        sleep 1
+        mkdir tempInstall
+        cd ./tempInstall
+    else 
+    mkdir tempInstall 
+    cd ./tempInstall
+fi
+}
+
+#make folder to downlaod
+checkFolder
+#begin to install. 
+echo  "Download the NodeExporter...." 
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
+sleep 1
+echo  "Extract the files..." 
+printf  "\n"
+tar -xvf node_exporter-1.1.2.linux-amd64.tar.gz
+sleep 2
+sudo mv ./node_exporter-*/node_exporter /usr/local/bin/
+sleep 2
+echo  "Create user for Node Exporter......"
+printf  "\n"
+sudo useradd -rs /bin/false node_exporter
+echo  "Clear installation files...."
+printf  "\n"
+cd ..
+rm -rf ./tempInstall
+sleep 2
+echo  "Waiting for something....."
+printf  "\n"
+echo  "Begin to setup services...."
+sleep 1
+cat > /etc/systemd/system/node_exporter.service <<EOL
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sleep 2
+printf  "\n"
+echo  "Configuration files added.. setup completed!...."
+clear
+echo "----------------------------------------------------------------------------------"
+echo " Configure service to run and test the services.... "
+echo "----------------------------------------------------------------------------------"
+sudo systemctl daemon-reload
+sleep 2
+printf  "\n"
+echo  "Try to start node_exporter services..."
+sudo systemctl start node_exporter
+sleep 2
+sudo systemctl status node_exporter
+sleep 1
+printf  "\n"
+printf  "\n"
+echo -e "\e[1;32m √ Please ensure the status is active before continue.... \e[0m"
+sleep 1
+read -p "Do you see the service status is active?. if Yes enter to continue..  "
+printf  "\n"
+echo  "Enable the exporter running automatically after reboot...."
+sudo systemctl enable node_exporter
+clear
+
+ipAddress=$(hostname -i)
+
+echo " Everthing complete!.. Time to configure prometheus with the node details..."
+echo "----------------------------------------------------------------------------------"
+echo "- job_name: 'node_exporter_metrics'"
+echo "  scrape_interval: 5s"
+echo "  static_configs:"
+echo "     - targets: ['$ipAddress:9100'] "
+echo "----------------------------------------------------------------------------------"
